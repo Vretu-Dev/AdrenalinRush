54d49f3ede93828a9209f8d05bca565d421a60f7
Vretu <82831707+Vretu-Dev@users.noreply.github.com>
2024-09-09 11:54:56 +0200

    Initial release

---
+++ AdrenalinRush/AdrenalinRush.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+
+namespace AdrenalinRush {
+    public class AdrenalinRush : Plugin<Config>
+    {
+        public override string Name => "Adrenalin Rush";
+        public override string Author => "Vretu";
+        public override string Prefix { get; } = "AR";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(8, 9, 8);
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.UsedItem += OnUsedItem;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.UsedItem -= OnUsedItem;
+            base.OnDisabled();
+        }
+
+        private void OnUsedItem(UsedItemEventArgs ev)
+        {
+            if (ev.Item.Type == ItemType.Adrenaline)
+            {
+                ev.Player.EnableEffect(EffectType.MovementBoost, Config.BoostIntensity, Config.BoostDuration);
+            }
+        }
+    }
+}
---
+++ AdrenalinRush/config.cs
@@ -0,0 +1,12 @@
+﻿using Exiled.API.Interfaces;
+
+namespace AdrenalinRush
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+        public int BoostDuration { get; set; } = 3;
+        public byte BoostIntensity { get; set; } = 30;
+    }
+}
